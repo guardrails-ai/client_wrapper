cb0100ca28f88113573c278a69a9e9b1feb85c7a
Alejandro <ae@alejandro.ltd>
2025-01-02 21:03:16 -0800

    renamed package name

--- client_wrapper/__init__.py
+++
@@ -1,5 +0,0 @@
-from client_wrapper.wrapper_funcs import tt_webhook_polling_sync
-
-__all__ = [
-    'tt_webhook_polling_sync',
-]
---
+++ guardrails_sim_client/__init__.py
@@ -0,0 +1,5 @@
+from guardrails_sim_client.wrapper_funcs import tt_webhook_polling_sync
+
+__all__ = [
+    'tt_webhook_polling_sync',
+]
