4c0b3a6a11b8a77b2b5b7903c583f66be5e0da3d
Caleb Courier <caleb.courier@gmail.com>
2025-01-28 14:21:05 -0600

    alias tt_webhook_polling_sync to simlab_connect

--- client_wrapper/__init__.py
+++ client_wrapper/__init__.py
@@ -1,9 +1,11 @@
 from client_wrapper.decorators.llm import tt_webhook_polling_sync
+from client_wrapper.decorators.llm import tt_webhook_polling_sync as simlab_connect
 from client_wrapper.decorators.custom_judge import custom_judge
 from client_wrapper.protocols import JudgeResult
 
 __all__ = [
-    'custom_judge',
-    'tt_webhook_polling_sync',
-    'JudgeResult'
+    "custom_judge",
+    "tt_webhook_polling_sync",
+    "JudgeResult",
+    "simlab_connect"
 ]
